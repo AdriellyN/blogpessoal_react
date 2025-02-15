1782dab4afe5bbdd6b405dcd2d477e2a92f55cca
AdriellyN <adriellynr@gmail.com>
2025-02-14 23:23:52 -0300

    :sparkles: Componentes NavBar, Home e Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import Home from "./pages/home/home"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/home/Home"
+import Login from "./components/login/Login"
+import Navbar from "./components/navbar/Navbar"
+import Footer from "./components/footer/Footer"
 
 function App() {
-  
+
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,25 @@
+function Footer() {
+    return (
+        <div className="w-full flex flex-col items-center bg-indigo-900 text-white py-4">
+            <div className="text-xl font-bold">
+                Blog Pessoal Adrielly | Copyright: 2025
+            </div>
+            <div className="">
+                Acesse minhas redes sociais
+            </div>
+            <div className="flex justify-center gap-2">
+                <a href="https://www.linkedin.com/in/adrielly-do-nascimento-ramos/">
+                    <img src="https://ik.imagekit.io/u4ctxhv0m/Icons/linkedin-logo-branco.svg?updatedAt=1739586157569" alt="Logo LinkedIn" />
+                </a>
+                <a href="https://github.com/AdriellyN">
+                    <img src="https://ik.imagekit.io/u4ctxhv0m/Icons/github-logo-branco.svg?updatedAt=1739586142607" alt="Logo Github" />
+                </a>
+                <a href="https://www.instagram.com/adriellyramos1/">
+                    <img src="https://ik.imagekit.io/u4ctxhv0m/Icons/instagram-logo-branco.svg?updatedAt=1739586128438" alt="Logo Instagram" />
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
