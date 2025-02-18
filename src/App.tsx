402a14cb9ea44e7aa61c467e48f627979248bcec
AdriellyN <adriellynr@gmail.com>
2025-02-17 21:53:08 -0300

    :sparkles: Login e Autenticação

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/home/Home"
-import Login from "./components/login/Login"
+import Login from "./pages/login/Login"
 import Navbar from "./components/navbar/Navbar"
 import Footer from "./components/footer/Footer"
+import Cadastro from "./pages/cadastro/Cadastro"
+import { AuthProvider } from "./contexts/AuthContext"
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <div className="min-h-[80vh]">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,29 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext"
 
 function Navbar() {
+
+    const navigate = useNavigate()
+
+    const {handleLogout } = useContext(AuthContext)
+
+    function logout(){
+        handleLogout()
+        alert('O Usuário foi desconectado com sucesso!')
+        navigate('/')
+    }
+
     return (
         <div className="w-full flex justify-center bg-gradient-to-t from-red-950 to-black text-white py-4" >
             <div className="container flex justify-between text-lg">
-                <Link to='/' className="text-2xl font-bold">Blog Pessoal</Link>
+                <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
                 <div className="flex gap-4">
                     Postagens
                     Temas
                     Cadastrar tema
                     Perfil
-                    <Link to='/login'> Sair</Link>
+                    <Link to='' onClick={logout} className="hover:underline"> Sair</Link>
                 </div>
             </div>
         </div>
