d731f8896a42eab61a6cab1097d93ebf394974f4
AdriellyN <adriellynr@gmail.com>
2025-02-20 21:44:17 -0300

    :sparkles: Implementação do recurso Postagem

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,9 +6,9 @@ function Navbar() {
 
     const navigate = useNavigate()
 
-    const {handleLogout } = useContext(AuthContext)
+    const { handleLogout } = useContext(AuthContext)
 
-    function logout(){
+    function logout() {
         handleLogout()
         alert('O Usuário foi desconectado com sucesso!')
         navigate('/')
@@ -19,7 +19,7 @@ function Navbar() {
             <div className="container flex justify-between text-lg">
                 <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
                 <div className="flex gap-4">
-                    Postagens
+                    <Link to='/postagens' className='hover:underline'>Postagens</Link>
                     <Link to='/temas' className='hover:underline'>Temas</Link>
                     <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
                     Perfil
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,33 +1,39 @@
+import ListaPostagens from "../../components/postagens/listapostagens/ListaPostagens"
+import ModalPostagem from "../../components/postagens/modalpostagem/ModalPostagem"
 
 function Home() {
+
     return (
-        <div className="flex justify-center bg-red-950">
-            <div className="container grid grid-cols-2 text-white">
-                <div className="flex flex-col items-center justify-center gap-4 py-4">
-                    <h2 className="text-5xl font-bold">Seja Bem Vinde!</h2>
-                    <p className="text-xl" >
-                        Expresse aqui seus pensamentos e
-                        opiniões
-                    </p>
-                    <div className="flex justify-around gap-4 ">
-                        <div className="rounded border-white border-solid border-2 py-2 px-4 text-white">
-                            Nova Postagem
+        <>
+            <div className="bg-red-950 flex justify-center">
+                <div className='container grid grid-cols-2 text-white'>
+                    <div className="flex flex-col gap-4 items-center
+                                    justify-center py-4">
+                        <h2 className='text-5xl font-bold'>
+                            Seja Bem Vinde!
+                        </h2>
+                        <p className='text-xl'>
+                            Expresse aqui seus pensamentos e opiniões
+                        </p>
+
+                        <div className="flex justify-around gap-4">
+                        <div className="flex justify-around gap-4">
+                                <ModalPostagem />
+                            </div>
                         </div>
                     </div>
-                </div>
 
-            
-
-
-                <div className="flex justify-content">
-                    <img
-                        src="https://ik.imagekit.io/u4ctxhv0m/Blog%20Pessoal/programmer%20steps.svg?updatedAt=1739587057865"
-                        alt="Imagem da Página Home"
-                        className="w-2/3"
-                    />
+                    <div id="imagem" className="flex justify-center">
+                        <img
+                            src="https://ik.imagekit.io/u4ctxhv0m/Blog%20Pessoal/programmer%20steps.svg?updatedAt=1739587057865"
+                            alt="Imagem da Página Home"
+                            className="w-2/3"
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
+            <ListaPostagens />
+        </>
     )
 }
 
