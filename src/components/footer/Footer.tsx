4d6529fca296e2c6f15c46cf89da4e2adda5ae0a
AdriellyN <adriellynr@gmail.com>
2025-02-21 16:18:46 -0300

    :sparkles: Construção da página de perfil!

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,24 +1,45 @@
+import { GithubLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { ReactNode, useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
+
 function Footer() {
+
+    let data = new Date().getFullYear()
+
+    const { usuario } = useContext(AuthContext)
+
+    let component: ReactNode
+
+    if (usuario.token !== "") {
+
+        component = (
+
+            <div className="flex justify-center bg-gradient-to-t from-black to-red-950 text-white">
+                <div className="container flex flex-col items-center py-4">
+                    <p className='text-xl font-bold'>
+                    Blog Pessoal Adrielly | Copyright: {data}
+                    </p>
+                    <p className='text-lg'>Acesse minhas redes sociais</p>
+                    <div className='flex gap-2'>
+                        <a href="https://www.linkedin.com/in/adrielly-do-nascimento-ramos/" target="_blank">
+                            <LinkedinLogo size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.instagram.com/adriellyramos1/" target="_blank">
+                            <InstagramLogo size={48} weight='bold' />
+                        </a>
+                        <a href="https://github.com/AdriellyN" target="_blank">
+                            <GithubLogo size={48} weight='bold' />
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
     return (
-        <div className="w-full flex flex-col items-center text-white py-4 bg-gradient-to-t from-black to-red-950">
-            <div className="text-xl font-bold">
-                Blog Pessoal Adrielly | Copyright: 2025
-            </div>
-            <div className="">
-                Acesse minhas redes sociais
-            </div>
-            <div className="flex justify-center gap-2">
-                <a href="https://www.linkedin.com/in/adrielly-do-nascimento-ramos/">
-                    <img src="https://ik.imagekit.io/u4ctxhv0m/Icons/linkedin-logo-branco.svg?updatedAt=1739586157569" alt="Logo LinkedIn" />
-                </a>
-                <a href="https://github.com/AdriellyN">
-                    <img src="https://ik.imagekit.io/u4ctxhv0m/Icons/github-logo-branco.svg?updatedAt=1739586142607" alt="Logo Github" />
-                </a>
-                <a href="https://www.instagram.com/adriellyramos1/">
-                    <img src="https://ik.imagekit.io/u4ctxhv0m/Icons/instagram-logo-branco.svg?updatedAt=1739586128438" alt="Logo Instagram" />
-                </a>
-            </div>
-        </div>
+        <>
+            { component }
+        </>
     )
 }
 
